add91290ee626bbb5b3ff15dc380c21f5214d6f6
owja87 <fandangoalfonse@gmail.com>
2017-07-30 19:25:05 +0200

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1ZxvU8eGyuN9M8GxTU9acKVJv70iC3px_m3EVFsOHN9g/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1K3iLYenzTj5o7Q5vLUP6dM_eQpEcCS1-_002QyasP9s/edit#gid=0';
