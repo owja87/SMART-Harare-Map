1950e5adca91d3e67ba3c459739569fa78691ccf
owja87 <fandangoalfonse@gmail.com>
2017-08-01 08:08:17 +0200

    Update map.js

--- scripts/map.js
+++ scripts/map.js
@@ -126,9 +126,8 @@ $(window).on('load', function() {
 
       if (point.Latitude !== '' && point.Longitude !== '') {
         var marker = L.marker([point.Latitude, point.Longitude], {icon: icon})
-          .bindPopup("<b>" + point['Name'] + '</b><br>' +
-          (point['Image'] ? ('<img src="' + point['Image'] + '"><br>') : '') +
-          point['Description']);
+          .bindPopup("<b>" + point['ID Report Title'] + '</b><br>' + point['Report Date'] + point['Council Response'] + point['Response Date']
+          (point['Image'] ? ('<img src="' + point['Image'] + '"><br>') : ''));
 
         if (layers !== undefined && layers.length !== 1) {
           marker.addTo(layers[point.Group]);
