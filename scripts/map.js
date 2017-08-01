705ad4d3ea7deb4cf584cde490d6f39dbd17dc35
owja87 <fandangoalfonse@gmail.com>
2017-08-01 07:52:08 +0200

    Update map.js

--- scripts/map.js
+++ scripts/map.js
@@ -70,7 +70,7 @@ $(window).on('load', function() {
     var layerNamesFromSpreadsheet = [];
     var layers = {};
     for (var i in points) {
-      var pointLayerNameFromSpreadsheet = points[i].Category;
+      var pointLayerNameFromSpreadsheet = points[i].Group;
       if (layerNamesFromSpreadsheet.indexOf(pointLayerNameFromSpreadsheet) === -1) {
         markerColors.push(
           points[i]['Marker Icon'].indexOf('.') > 0
@@ -131,7 +131,7 @@ $(window).on('load', function() {
           point['Description']);
 
         if (layers !== undefined && layers.length !== 1) {
-          marker.addTo(layers[point.Category]);
+          marker.addTo(layers[point.Group]);
         }
 
         markerArray.push(marker);
